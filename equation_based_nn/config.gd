6d5258bfafc6494d247a3f15d1ab96621333826f
ArchBase <archbase557@gmail.com>
2024-04-28 11:04:37 +0530

    better to not to use json

---
+++ equation_based_nn/config.gd
@@ -0,0 +1,12 @@
+extends Node
+
+static var config = {}
+
+func _ready():
+	config = {
+	}
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
