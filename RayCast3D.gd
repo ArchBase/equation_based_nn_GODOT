19726170969b1ca67529bc70703217d1d212b20a
ArchBase <archbase557@gmail.com>
2024-04-29 20:39:31 +0530

    better to change architecture

--- RayCast3D.gd
+++ RayCast3D.gd
@@ -3,20 +3,19 @@ extends RayCast3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var camera = $look/Camera3D
+	var view = get_viewport()
+	#print(view.camera)
 	pass # Replace with function body.
 
 var bonet = false
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if is_colliding():
-		print("colliding")
+		#print("colliding")
+		pass
 	else:
-		print("not colliding")
-	if bonet:
-		print("bonet hit")
-	else:
-		print("bonet not hit")
+		#print("not colliding")
+		pass
 
 
-func _on_bonet_tree_entered():
-	print("bonet hit")
